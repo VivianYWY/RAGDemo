db566d01e81d7090db5fe92383657e5f09b62e60
VivianYWY <31574124+VivianYWY@users.noreply.github.com>
2026-03-20 08:46:37 +0800

    Create example1.py

---
+++ example1.py
@@ -0,0 +1,6 @@
+from langchain_community.document_loaders import PyPDFLoader
+pdf_path = "sample_rag_document.pdf"
+loader = PyPDFLoader(pdf_path)
+documents = loader.load()
+print(f"成功加载了{len(documents)}页文档。")
+print(f"第一页的元数据：{documents[0].metadata}")
